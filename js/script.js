f6c5fa09d28c42f2218c0ee28884176c1300fab4
Oblv49 <noprivacyworld@gmail.com>
2023-11-27 14:52:40 +0100

    work page ok but no all interaction is active

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const aboutContainer = document.getElementById('about');
 const homepageContainer = document.getElementById('homepage');
+const workContainer = document.getElementById('work');
 const homepageLink = document.getElementById('homepageLink');
 const aboutLink = document.getElementById('aboutLink');
+const workLink = document.getElementById('workLink');
 const navbarToggle = document.querySelector('.navbar-toggler');
 const navbarNav = document.getElementById('navbarNav');
 
@@ -11,29 +13,60 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /*HOMEPAGE*/
 homepageLink.addEventListener('click', function () {
-    if (!aboutContainer.classList.contains('d-none')) {
-        aboutContainer.classList.add('d-none');
-        homepageContainer.classList.remove('d-none');
+    if (!aboutContainer.classList.contains('d-none') || !workContainer.classList.contains('d-none')) {
+        if (!aboutContainer.classList.contains('d-none')) {
+            aboutContainer.classList.add('d-none');
+            homepageContainer.classList.remove('d-none');
+        } else if (!workContainer.classList.contains('d-none')) {
+            workContainer.classList.add('d-none');
+            homepageContainer.classList.remove('d-none');
+        }    
+        
         AOS.init();
-        // Aggiungi la classe 'active' al link corrente e rimuovi da altri link
+
         homepageLink.classList.add('active');
         aboutLink.classList.remove('active');
-        // Chiudi il menu su mobile
+        workLink.classList.remove('active');
+        navbarNav.classList.remove('show');
+    }
+});
+
+/* WORK */
+workLink.addEventListener('click', function () {
+    if (!homepageContainer.classList.contains('d-none') || !aboutContainer.classList.contains('d-none')) {
+        if (!homepageContainer.classList.contains('d-none')) {
+            homepageContainer.classList.add('d-none');
+            workContainer.classList.remove('d-none');
+        } else if (!aboutContainer.classList.contains('d-none')) {
+            aboutContainer.classList.add('d-none');
+            workContainer.classList.remove('d-none');
+        }
+
+        AOS.init();
+
+        workLink.classList.add('active');
+        homepageLink.classList.remove('active');
+        aboutLink.classList.remove('active');
         navbarNav.classList.remove('show');
     }
 });
 
 /*ABOUT*/
 aboutLink.addEventListener('click', function () {
-    if (!homepageContainer.classList.contains('d-none')) {
-
-        homepageContainer.classList.add('d-none');
-        aboutContainer.classList.remove('d-none');
+    if (!homepageContainer.classList.contains('d-none') || !workContainer.classList.contains('d-none')) {
+        if (!homepageContainer.classList.contains('d-none')) {
+            homepageContainer.classList.add('d-none');
+            aboutContainer.classList.remove('d-none');
+        } else if (!workContainer.classList.contains('d-none')) {
+            workContainer.classList.add('d-none');
+            aboutContainer.classList.remove('d-none');
+        }
 
         AOS.init();
 
         aboutLink.classList.add('active');
         homepageLink.classList.remove('active');
+        workLink.classList.remove('active');
         navbarNav.classList.remove('show');
     }
 });
